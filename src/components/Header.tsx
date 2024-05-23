f150a69471900f3462fb2de8aae91b6a26bf9c0b
Eliz Erdim <elizerdim@gmail.com>
2024-05-23 23:16:59 +0300

    Add content to the Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,13 @@
+import { useContext } from "react";
+import { ColorModeContext } from "../context/ColorModeContext";
+
 export default function Header() {
+  const { colorMode, toggleColorMode } = useContext(ColorModeContext);
+
   return (
-    <div>Header</div>
-  )
+    <header>
+      <h1>Where in the world?</h1>
+      <button onClick={toggleColorMode}>{colorMode === "light" ? "Dark" : "Light"} Mode</button>
+    </header>
+  );
 }
