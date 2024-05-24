93e96e683fbec46252dc0ac87767eb714fc5d24f
Eliz Erdim <elizerdim@gmail.com>
2024-05-24 15:28:40 +0300

    Add Header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,25 @@
 import { useContext } from "react";
 import { ColorModeContext } from "../context/ColorModeContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-regular-svg-icons";
+import { faSun } from "@fortawesome/free-regular-svg-icons";
 
 export default function Header() {
   const { darkMode, toggleColorMode } = useContext(ColorModeContext);
 
   return (
     <header data-theme={darkMode ? "dark" : ""}>
-      <h1>Where in the world?</h1>
-      <button onClick={toggleColorMode}>{darkMode ? "Dark" : "Light"} Mode</button>
+      <div className="container">
+        <h1 className="fs-14-24 fw-800">Where in the world?</h1>
+        <button className="fs-12-16 fw-600" onClick={toggleColorMode}>
+          {darkMode ? (
+            <FontAwesomeIcon icon={faSun} className="mode-btn-icon" />
+          ) : (
+            <FontAwesomeIcon icon={faMoon} className="mode-btn-icon" />
+          )}
+          {darkMode ? "Light" : "Dark"} Mode
+        </button>
+      </div>
     </header>
   );
 }
