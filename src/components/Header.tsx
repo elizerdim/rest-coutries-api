5579937184e29d974b897b299fff8c782810060d
Eliz Erdim <elizerdim@gmail.com>
2024-05-25 14:20:03 +0300

    Add bg-accent class to main header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   const { darkMode, toggleColorMode } = useContext(ColorModeContext);
 
   return (
-    <header className="main-header" data-theme={darkMode ? "dark" : ""}>
+    <header className="main-header bg-accent" data-theme={darkMode ? "dark" : ""}>
       <div className="container">
         <h1 className="fs-14-24 fw-800">Where in the world?</h1>
         <button className="fs-12-16 fw-600 clr-mode-btn" onClick={toggleColorMode}>
