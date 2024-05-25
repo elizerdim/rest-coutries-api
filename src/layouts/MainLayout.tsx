a4ad4995686712ca49e8aeb78afb7d36fed5abca
Eliz Erdim <elizerdim@gmail.com>
2024-05-25 14:45:41 +0300

    Add background color to MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import { useContext } from "react";
+import { ColorModeContext } from "../context/ColorModeContext";
 
 export default function MainLayout() {
+  const { darkMode } = useContext(ColorModeContext);
+
   return (
-    <>
+    <div className="bg-main" data-theme={darkMode ? "dark" : ""}>
       <Header />
       <Outlet />
-    </>
-  )
+    </div>
+  );
 }
