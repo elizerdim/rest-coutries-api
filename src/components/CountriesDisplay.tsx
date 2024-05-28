3071e509e42d3f81f1862e6a9b9d77fe447bbf96
Eliz Erdim <elizerdim@gmail.com>
2024-05-28 20:37:20 +0300

    Add styles to country cards

--- src/components/CountriesDisplay.tsx
+++ src/components/CountriesDisplay.tsx
@@ -8,22 +8,25 @@ type CountriesDisplayProps = {
 export default function CountriesDisplay({ countries }: CountriesDisplayProps) {
   return (
     // TODO: move .toLowerCase().split(" ").join("-") logic to a util function file
+    // TODO: make the cards block links https://css-tricks.com/block-links-the-search-for-a-perfect-solution/
     <div className="display-container container text-clr">
       {countries.map((country) => (
         <article
           className="country-card bg-accent"
           key={country.name.official.toLowerCase().split(" ").join("-")}
         >
-          <img src={country.flags.png} alt={country.flags.alt} />
+          <img className="country-flag" src={country.flags.png} alt={country.flags.alt} />
           <Link
             to={`/${country.name.common.toLowerCase().split(" ").join("-")}`}
             className="country-name block fs-18 fw-800"
           >
             {country.name.common}
           </Link>
-          <p>Population: {country.population}</p>
-          <p>Region: {country.region}</p>
-          <p>Capital: {country.capital?.[0] || "None"}</p>
+          <div className="country-info">
+            <p><span className="fw-600">Population:</span> {country.population}</p>
+            <p><span className="fw-600">Region:</span> {country.region}</p>
+            <p><span className="fw-600">Capital:</span> {country.capital?.[0] || "None"}</p>
+          </div>
         </article>
       ))}
     </div>
