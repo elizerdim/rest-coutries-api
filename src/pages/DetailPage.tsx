262071aa5cca5e87d9253d49e8c22aea5bf82d08
Eliz Erdim <elizerdim@gmail.com>
2024-05-28 22:35:37 +0300

    Add back button to DetailPage

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,11 +1,20 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData } from "react-router-dom";
 import Country from "../interfaces/Country";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
 
 export default function DetailPage() {
   const country: Country = useLoaderData() as Country;
   console.log(country);
 
   return (
-    <div>{country.name.common}</div>
-  )
+    <main>
+      <div className="container">
+        <button className="back-btn bg-accent fs-14-16 text-clr">
+          <FontAwesomeIcon className="back-btn-icon" icon={faArrowLeftLong} />
+          Back
+        </button>
+      </div>
+    </main>
+  );
 }
