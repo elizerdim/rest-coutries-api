a02f7b4d085c612b91319996450793fa7c62fd54
Eliz Erdim <elizerdim@gmail.com>
2024-05-29 21:31:55 +0300

    Create the layout of DetailPage for wider screens

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -25,74 +25,88 @@ export default function DetailPage({ countries }: DetailPageProps) {
   // TODO: Add click functionality to back button
   // TODO: Make back button a tag instead of button tag
   return (
-    <main className="text-clr fw-300">
-      <div className="country-details-container container">
+    <main className="container text-clr fw-300">
+      <div className="country-details-container">
         <button className="back-btn bg-accent fs-14-16 text-clr">
           <FontAwesomeIcon className="back-btn-icon" icon={faArrowLeftLong} />
           Back
         </button>
-        <img
-          className="country-details__flag"
-          src={country.flags.png}
-          alt={country.flags.alt}
-        />
-        <h2 className="country-details__title fw-800 fs-22-32">{country.name.common}</h2>
-        <div className="country-details__info-col">
-          <p className="country-details__info fs-14-16">
-            <span className="fw-600">Native Name: </span>
-            {nativeName || <span className="italic">None</span>}
-          </p>
-          <p className="country-details__info fs-14-16">
-            <span className="fw-600">Population: </span>
-            {country.population.toLocaleString()}
-          </p>
-          <p className="country-details__info fs-14-16">
-            <span className="fw-600">Region: </span>
-            {country.region}
-          </p>
-          <p className="country-details__info fs-14-16">
-            <span className="fw-600">Sub Region: </span>
-            {country?.subregion || <span className="italic">None</span>}
-          </p>
-          <p className="country-details__info fs-14-16">
-            <span className="fw-600">Capital: </span>
-            {country.capital?.[0] || <span className="italic">None</span>}
-          </p>
-        </div>
-        <div className="country-details__info-col fs-14-16">
-          <p className="country-details__info ">
-            <span className="fw-600">Top Level Domain: </span>
-            {country.tld?.[0] || <span className="italic">None</span>}
-          </p>
-          <p className="country-details__info ">
-            <span className="fw-600">Currencies: </span>
-            {currencies.join(", ") || <span className="italic">None</span>}
-          </p>
-          <p className="country-details__info ">
-            <span className="fw-600">Languages: </span>
-            {langauges.join(", ") || <span className="italic">None</span>}
-          </p>
-        </div>
-        <p className="country-details__borders fw-600 fs-16">
-          Border Countries:{" "}
-        </p>
-        <div className="country-details__border-links fw-300 fs-12-14">
-          {borderCountries?.map((borderCountry) => (
-            <Link
-              className="country-details__border-link bg-accent"
-              to={`/${borderCountry.name.common
-                .toLowerCase()
-                .split(" ")
-                .join("-")}`}
-              key={borderCountry.name.official
-                .toLowerCase()
-                .split(" ")
-                .join("-")}
-              aria-label={`Go to ${borderCountry.name.common}'s page`}
-            >
-              {borderCountry.name.common}
-            </Link>
-          )) || <span className="italic">None</span>}
+        <div className="country-details-grid">
+          <img
+            className="country-details__flag"
+            src={country.flags.png}
+            alt={country.flags.alt}
+          />
+          <div className="country-details__right-col">
+            <h2 className="country-details__title fw-800 fs-22-32">
+              {country.name.common}
+            </h2>
+            <div className="country-details__info-flex">
+              <div className="country-details__info-col">
+                <p className="country-details__info fs-14-16">
+                  <span className="fw-600">Native Name: </span>
+                  {nativeName || <span className="italic">None</span>}
+                </p>
+                <p className="country-details__info fs-14-16">
+                  <span className="fw-600">Population: </span>
+                  {country.population.toLocaleString()}
+                </p>
+                <p className="country-details__info fs-14-16">
+                  <span className="fw-600">Region: </span>
+                  {country.region}
+                </p>
+                <p className="country-details__info fs-14-16">
+                  <span className="fw-600">Sub Region: </span>
+                  {country?.subregion || <span className="italic">None</span>}
+                </p>
+                <p className="country-details__info fs-14-16">
+                  <span className="fw-600">Capital: </span>
+                  {country.capital?.[0] || <span className="italic">None</span>}
+                </p>
+              </div>
+              <div className="country-details__info-col fs-14-16">
+                <p className="country-details__info ">
+                  <span className="fw-600">Top Level Domain: </span>
+                  {country.tld?.[0] || <span className="italic">None</span>}
+                </p>
+                <p className="country-details__info ">
+                  <span className="fw-600">Currencies: </span>
+                  {currencies.join(", ") || (
+                    <span className="italic">None</span>
+                  )}
+                </p>
+                <p className="country-details__info ">
+                  <span className="fw-600">Languages: </span>
+                  {langauges.join(", ") || <span className="italic">None</span>}
+                </p>
+              </div>
+            </div>
+            <p className="country-details__borders fw-600 fs-16">
+              Border Countries:{" "}
+            </p>
+            <div className="country-details__border-links fw-300 fs-12-14">
+              {borderCountries.length > 0 ? (
+                borderCountries?.map((borderCountry) => (
+                  <Link
+                    className="country-details__border-link bg-accent"
+                    to={`/${borderCountry.name.common
+                      .toLowerCase()
+                      .split(" ")
+                      .join("-")}`}
+                    key={borderCountry.name.official
+                      .toLowerCase()
+                      .split(" ")
+                      .join("-")}
+                    aria-label={`Go to ${borderCountry.name.common}'s page`}
+                  >
+                    {borderCountry.name.common}
+                  </Link>
+                ))
+              ) : (
+                <span className="italic">None</span>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </main>
