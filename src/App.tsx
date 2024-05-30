f9796c2b1b909588b36360e4db0e25bb77ce83e2
Eliz Erdim <elizerdim@gmail.com>
2024-05-31 02:44:52 +0300

    Add TODO comments

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { useEffect, useState } from "react";
 import Country from "./interfaces/Country";
 import countryLoader from "./utils/countryLoader";
 
+// TODO: Add loading states
 export default function App() {
   const [countries, setCountries] = useState<Country[] | []>([]);
 
--- src/utils/countryLoader.ts
+++ src/utils/countryLoader.ts
@@ -4,6 +4,8 @@ import {
 import Country from "../interfaces/Country";
 import { kebabCase } from "lodash";
 
+// TODO: Figure out what this error is about - it says 'loader'
+// Error: You defined a loader for route "0-1" but didn't return anything from your `loader` function. Please return a value or `null`.
 const countryLoader = (countries: Country[], {params}: LoaderFunctionArgs): Country => {
   const country = countries.find(
     (c) => kebabCase(c.name.common) === params.id
