b73fa5e87bbd187cf7e4072fc71801bedf436770
Eliz Erdim <elizerdim@gmail.com>
2024-05-30 22:04:51 +0300

    Add TODO comment for dark mode logic

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -7,6 +7,7 @@ export default function MainLayout() {
   const { darkMode } = useContext(ColorModeContext);
 
   return (
+  // TODO: Can I move color code logic here with the useState instead of using this data-theme all around the code?
     <div className="layout-wrapper bg-main" data-theme={darkMode ? "dark" : ""}>
       <Header />
       <Outlet />
