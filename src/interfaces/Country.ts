862fa0c882fd96de64b71769f8708e2850e57f64
Eliz Erdim <elizerdim@gmail.com>
2024-05-31 05:04:05 +0300

    Update the Country interface

--- src/interfaces/Country.ts
+++ src/interfaces/Country.ts
@@ -1,21 +1,15 @@
-// TODO: Review this with the API and the code that uses it 
 export default interface Country {
   name: {
     common: string;
-    official: string;
     nativeName?: {
       [propName: string]: {
         common: string;
-        official: string;
       };
     };
-    // TODO: Should I include this line?
-    [propName: string]: string | object | undefined;
   };
   region: string;
   flags: {
     png: string;
-    svg: string;
     alt: string;
   };
   population: number;
@@ -28,11 +22,8 @@ export default interface Country {
   currencies?: {
     [propName: string]: {
       name: string;
-      symbol: string;
     };
   };
   borders?: string[];
   cca3: string;
-  // TODO: Should I include this line?
-  [propName: string]: string | boolean | number | object | undefined;
 }
